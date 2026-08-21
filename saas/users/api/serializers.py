ccdbb6ca77b94778224af139aa76529b80de3abb
0ankit0 <ankitpaudel.1373@gmail.com>
2026-08-21 11:35:59 +0545

    updated user view and serializer to properly integrate the hashids for id

--- saas/users/api/serializers.py
+++ saas/users/api/serializers.py
@@ -1,13 +1,12 @@
 from rest_framework import serializers
+from django.contrib.auth.models import Group, Permission
 
+from saas.contrib.fields import HashIDField
 from saas.users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer[User]):
+    id = HashIDField()
     class Meta:
         model = User
-        fields = ["name", "url"]
-
-        extra_kwargs = {
-            "url": {"view_name": "api:user-detail", "lookup_field": "pk"},
-        }
+        fields = [ "id", "email", "is_active", "is_staff", "is_superuser"]
--- saas/users/api/views.py
+++ saas/users/api/views.py
@@ -1,11 +1,16 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin
 from rest_framework.mixins import RetrieveModelMixin
 from rest_framework.mixins import UpdateModelMixin
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from django.contrib.auth.models import Group, Permission
+from werkzeug.exceptions import NotFound
+from saas.contrib.views import HashIDModelViewSet
 
+from saas.contrib.utils import decode_hashid
 from saas.users.models import User
 
 from .serializers import UserSerializer
@@ -14,7 +19,26 @@ from .serializers import UserSerializer
 class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    lookup_field = "pk"
+    lookup_field = "ukid"
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        ukid = self.kwargs[
+            self.lookup_url_kwarg or self.lookup_field
+        ]
+
+
+        try:
+            pk = decode_hashid(ukid)
+        except ValueError:
+            raise NotFound("Invalid ID.")
+
+        obj = get_object_or_404(queryset, pk=pk)
+
+        self.check_object_permissions(self.request, obj)
+
+        return obj
 
     def get_queryset(self, *args, **kwargs):
         assert isinstance(self.request.user.id, int)
